4401bb9239a43423c8b54fb755a86d3fc13df4b4
Pruthviraj Chauhan <chauhanpruthviraj309@gmail.com>
2024-08-12 09:04:26 +0530

    Update LAB_6.sql

--- LAB_6.sql
+++ LAB_6.sql
@@ -1,3 +1,5 @@
+--	Part-A
+
 -- 1. Display the result of 5 multiply by 30.
 SELECT 5 * 30 AS result;
 
@@ -37,14 +39,14 @@ SELECT
   EXP(3) AS ExponentialOf3;
 
 --12. Find out logarithm having base e of 10 and 2.
-SELECT 
+SELECT
   LOG(10) AS NaturalLogOf10,
   LOG(2) AS NaturalLogOf2;
 
 --13. Find out logarithm having base b having value 10 of 5 and 100.
 SELECT 
-  LOG(5, 10) AS LogBase10Of5,
-  LOG(100, 10) AS LogBase10Of100;
+  LOG10(5) AS LogBase10Of5,
+  LOG10(10) AS LogBase10Of100;
 
 --14. Find sine, cosine and tangent of 3.1415.
 SELECT 
@@ -60,3 +62,148 @@ SELECT
 
 --16. Generate random number using function
 SELECT RAND() AS RandomNumber;
+
+--	Part-B
+--Create and Insert the following records in the EMP_MASTER table
+
+DROP TABLE EMP_MASTER
+CREATE TABLE EMP_MASTER(
+	EmpNo INT,
+	EmpName VARCHAR(25),
+	JoiningDate DATE,
+	Salary DECIMAL(7, 2),
+	Commission INT,
+	City VARCHAR(50),
+	Dept_Code VARCHAR(50)
+);
+
+
+INSERT INTO EMP_MASTER VALUES
+(101, 'Keyur', '2000-01-05', 12000.00, 4500, 'Rajkot', '3@g'),
+(102, 'Hardik', '2004-02-15', 14000.00, 2500, 'Ahmedabad', '3@'),
+(103, 'Kajal', '2006-03-14', 15000.00, 3000, 'Baroda', '3-GD'),
+(104, 'Bhoomi', '2005-06-23', 12500.00, 1000, 'Ahmedabad', '1A3D'),
+(105, 'Harmit', '2004-02-15', 14000.00, 2000, 'Rajkot', '312A');
+
+--1. Display the result of Salary plus Commission.
+--2. Find smallest integer value that is greater than or equal to 55.2, 35.7 and -55.2.
+--3. Find largest integer value that is smaller than or equal to 55.2, 35.7 and -55.2.
+--4. Find out remainder of 55 divided 2 and 55 divided by 3.
+--5. Find out value of 23 raised to 2nd power and 14 raised 3rd power
+
+
+------------------------------------------------------------------------------STRING FUNCTIONS----------------------------------------------------------------------------------------
+--	PART-A
+--1. Find the length of following. (I) NULL (II) ‘ hello ’ (III) Blank
+SELECT
+	LEN(NULL),
+	LEN('hello'),
+	LEN('blank');
+--2. Display your name in lower & upper case.
+SELECT
+	UPPER('CHAUHAN PRUTHVIRAJ'),
+	LOWER('CHAUHAN PRUTHVIRAJ');
+--3. Display first three characters of your name.
+SELECT
+	LEFT('CHAUHAN PRUTHVIRAJ', 3)
+	--RIGHT('CHAUHAN PRUTHVIRAJ', );
+--4. Display 3rd to 10th character of your name.
+SELECT
+	SUBSTRING('CHAUHAN PRUTHVIRAJ', 3, 10);
+--5. Write a query to convert ‘abc123efg’ to ‘abcXYZefg’ & ‘abcabcabc’ to ‘ab5ab5ab5’ using REPLACE.
+SELECT
+	REPLACE('abc123efg', 'abc123efg','abcXYZefg'),
+	REPLACE('abcabcabc', 'abcabcabc','ab5ab5ab5')
+--6. Write a query to display ASCII code for ‘a’,’A’,’z’,’Z’, 0, 9.
+SELECT
+	ASCII('a'),
+	ASCII('A'),
+	ASCII('z'),
+	ASCII('Z'),
+	ASCII('0'),
+	ASCII('9');
+--7. Write a query to display character based on number 97, 65,122,90,48,57.
+SELECT
+	CHAR(97),
+	CHAR(65),
+	CHAR(122),
+	CHAR(90),
+	CHAR(48),
+	CHAR(57);
+--8. Write a query to remove spaces from left of a given string ‘hello world ‘.
+SELECT
+	LTRIM('     hello world');
+--9. Write a query to remove spaces from right of a given string ‘ hello world ‘.
+SELECT
+	RTRIM('hello world      ');
+--10. Write a query to display first 4 & Last 5 characters of ‘SQL Server’.
+SELECT
+	LEFT('SQL Server', 4),
+	RIGHT('SQL Server', 5);
+--11. Write a query to convert a string ‘1234.56’ to number (Use cast and convert function).
+SELECT
+	CAST('123.45' AS decimal(10, 2));
+--12. Write a query to convert a float 10.58 to integer (Use cast and convert function).
+SELECT
+	CAST(10.58 AS INT);
+--13. Put 10 space before your name using function.
+SELECT
+	SPACE(10) + 'PRUTHVIRAJ';
+--14. Combine two strings using + sign as well as CONCAT ().
+SELECT
+	CONCAT('CHAUHAN', ' PRUTHVIRAJ'),
+	'CHAUHAN'+' PRUTHVIRAJ';
+--15. Find reverse of “Darshan”.
+SELECT
+	REVERSE('Darshan');
+--16. Repeat your name 3 times.
+SELECT
+	REPLICATE('CHAUHAN', 3);
+
+--Date Functions
+--Part – A:
+--1. Write a query to display the current date & time. Label the column Today_Date.
+SELECT
+	GETDATE();
+--2. Write a query to find new date after 365 day with reference to today.
+SELECT
+	DATEADD(YEAR, 1, GETDATE());
+--3. Display the current date in a format that appears as may 5 1994 12:00AM.
+SELECT
+	CONVERT(VARCHAR, GETDATE(), 100);
+--4. Display the current date in a format that appears as 03 Jan 1995.
+SELECT
+	CONVERT(VARCHAR, GETDATE(), 13);
+--5. Display the current date in a format that appears as Jan 04, 96.
+SELECT
+	CONVERT(VARCHAR, GETDATE(), 7);
+--6. Write a query to find out total number of months between 31-Dec-08 and 31-Mar-09.
+SELECT
+	DATEDIFF(MM, '31-Dec-2008', '31-Mar-2009');
+--7. Write a query to find out total number of years between 25-Jan-12 and 14-Sep-10.
+SELECT
+	DATEDIFF(yy, '14-Sep-10', '25-Jan-12');
+--8. Write a query to find out total number of hours between 25-Jan-12 7:00 and 26-Jan-12 10:30.
+SELECT
+	DATEDIFF(HH, '25-Jan-12 7:00', '26-Jan-12 10:30');
+--9. Write a query to extract Day, Month, Year from given date 12-May-16.
+SELECT
+	DATEPART(dd, '12-May-16'),
+	DATEPART(MM, '12-May-16'),
+	DATEPART(yy, '12-May-16');
+--10. Write a query that adds 5 years to current date.
+SELECT
+	DATEADD(yy, 5, GETDATE());
+--11. Write a query to subtract 2 months from current date.
+SELECT
+	DATEADD(MM, -2, GETDATE());
+--12. Extract month from current date using datename () and datepart () function.
+SELECT
+	DATENAME(MM, GETDATE()),
+	DATEPART(MM, GETDATE());
+--13. Write a query to find out last date of current month.
+SELECT
+	EOMONTH(GETDATE());
+--14. Calculate your age in years and months.
+SELECT
+	DATEDIFF(yy, '24-Dec-05', GETDATE());
